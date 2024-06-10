de091ca4a3d1bc707f200a6ac1a2576e57db88f9
kavindu919 <jayakodyk2@gmail.com>
2024-06-10 12:20:57 +0530

    make navbar

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -1,8 +1,48 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Navbar.css";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  //make state for the active link
+  const [menu, setMenu] = useState("Home");
+  return (
+    <div className="navbar">
+      <img src="logo.png" alt="logo" width={90} className="logo" />
+      <ul className="navbar-menu">
+        <li
+          onClick={() => setMenu("Home")}
+          className={menu === "Home" ? "active" : ""}
+        >
+          Home
+        </li>
+        <li
+          onClick={() => setMenu("Menu")}
+          className={menu === "Menu" ? "active" : ""}
+        >
+          Menu
+        </li>
+        <li
+          onClick={() => setMenu("About")}
+          className={menu === "About" ? "active" : ""}
+        >
+          About
+        </li>
+        <li
+          onClick={() => setMenu("  Contact Us")}
+          className={menu === "  Contact Us" ? "active" : ""}
+        >
+          Contact Us
+        </li>
+      </ul>
+      <div className="navbar-right">
+        <img src="search_icon.png" alt="Search Icon" />
+        <div className="navbar-search-icon">
+          <img src="basket_icon.png" alt="basket" />
+          <div className="dot"></div>
+        </div>
+        <button>Sign in</button>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
