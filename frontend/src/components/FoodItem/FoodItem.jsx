a3279aa72b8f9693849fd15bc9550c7ed4b307a5
kavindu919 <jayakodyk2@gmail.com>
2024-06-12 16:04:21 +0530

    make food display and food item components

---
+++ frontend/src/components/FoodItem/FoodItem.jsx
@@ -0,0 +1,47 @@
+import React, { useState } from "react";
+import "./FoodItem.css";
+import { assets } from "../../assets/assets";
+
+const FoodItem = ({ id, name, price, description, image }) => {
+  //create usestate for cart
+  const [itemCount, setItemCount] = useState(0);
+  return (
+    <div className="food-item">
+      <div className="food-item-img-container">
+        <img className="food-item-image" src={image} alt="foodimage" />
+        {!itemCount ? (
+          <img
+            className="add"
+            onClick={() => setItemCount((prev) => prev + 1)}
+            src={assets.add_icon_white}
+            alt="add icon"
+          />
+        ) : (
+          <div className="food-item-counter">
+            <img
+              onClick={() => setItemCount((prev) => prev - 1)}
+              src={assets.remove_icon_red}
+              alt="remove icon"
+            />
+            <p>{itemCount}</p>
+            <img
+              onClick={() => setItemCount((prev) => prev + 1)}
+              src={assets.add_icon_green}
+              alt="add icon green"
+            />
+          </div>
+        )}
+      </div>
+      <div className="food-item-info">
+        <div className="food-item-name-rating">
+          <p>{name}</p>
+          <img src={assets.rating_starts} alt="" />
+        </div>
+        <p className="food-item-description">{description}</p>
+        <p className="food-item-price">$ {price}</p>
+      </div>
+    </div>
+  );
+};
+
+export default FoodItem;
