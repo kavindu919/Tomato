c559bac4e56456867bc2fe693fc92084fe5236ab
kavindu919 <jayakodyk2@gmail.com>
2024-06-13 15:56:17 +0530

    make responsive the design

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import "./Navbar.css";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
   //make state for the active link
@@ -8,30 +9,33 @@ const Navbar = () => {
     <div className="navbar">
       <img src="logo.png" alt="logo" width={90} className="logo" />
       <ul className="navbar-menu">
-        <li
+        <Link
+          to="/"
           onClick={() => setMenu("Home")}
           className={menu === "Home" ? "active" : ""}
         >
           Home
-        </li>
-        <li
+        </Link>
+        <a
+          href="#explore-menu"
           onClick={() => setMenu("Menu")}
           className={menu === "Menu" ? "active" : ""}
         >
           Menu
-        </li>
-        <li
+        </a>
+        <a
           onClick={() => setMenu("About")}
           className={menu === "About" ? "active" : ""}
         >
           About
-        </li>
-        <li
+        </a>
+        <a
+          href="#footer"
           onClick={() => setMenu("  Contact Us")}
           className={menu === "  Contact Us" ? "active" : ""}
         >
           Contact Us
-        </li>
+        </a>
       </ul>
       <div className="navbar-right">
         <img src="search_icon.png" alt="Search Icon" />
